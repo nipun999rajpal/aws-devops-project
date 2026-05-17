0501b7e5090ce76fb7673ca073d1c7f0d2b39ae5
nipun999rajpal <nipunrajpal999@gmail.com>
2026-05-17 18:13:21 +0530

    Completed Phase 2 production-grade DevOps enhancements

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "nipun-terraform-state-bucket"
+    key            = "devops-project/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
